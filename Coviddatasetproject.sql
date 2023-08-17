1bdb5a4d5e1c69dbd8337e930d0894a31dc0929d
Harish.D <123487934+HarishDeepak@users.noreply.github.com>
2023-08-17 19:46:02 +0530

    Update Coviddatasetproject.sql
    Formatted sql file

--- Coviddatasetproject.sql
+++ Coviddatasetproject.sql
@@ -1,154 +1,129 @@
----
-SELECT location,date,total_cases,total_deaths,population
+-- Selecting specific columns from CovidDeaths table and ordering by location and date
+SELECT location, date, total_cases, total_deaths, population
 FROM [dbo].[CovidDeaths]
-order by 1,2
+ORDER BY 1, 2;
 
+-- Selecting all columns from CovidDeaths table where continent is not null and ordering by location and date
 SELECT *
 FROM [dbo].[CovidDeaths]
-where continent is not null
-order by 1,2
+WHERE continent IS NOT NULL
+ORDER BY 1, 2;
 
---total deaths vs cases
---the prob of dying if we get covid
-SELECT location,date,total_cases,total_deaths, (total_deaths/total_cases)*100 as deathpercentage
+-- Calculating death percentage for COVID cases in India and ordering by location and date
+SELECT location, date, total_cases, total_deaths, (total_deaths / total_cases) * 100 AS deathpercentage
 FROM [dbo].[CovidDeaths]
-Where location like '%india%'
-order by 1,2
+WHERE location LIKE '%india%'
+ORDER BY 1, 2;
 
---total cases vs population
---shows the percentage of population which got covid
-SELECT location,date,total_cases,population, (total_cases/population)*100 as percentage
+-- Calculating COVID cases percentage of population in India and ordering by location and date
+SELECT location, date, total_cases, population, (total_cases / population) * 100 AS percentage
 FROM [dbo].[CovidDeaths]
-Where location like '%india%'
-order by 1,2
+WHERE location LIKE '%india%'
+ORDER BY 1, 2;
 
---countries with highest infection RATE VS POPULATION
-SELECT location,population,MAX(total_cases) as HighestInfectionCount,
-MAX((total_cases/population)*100) as maxinfpercentage
+-- Finding countries with the highest infection rate vs population and ordering by the maximum infection percentage in descending order
+SELECT location, population, MAX(total_cases) AS HighestInfectionCount,
+MAX((total_cases / population) * 100) AS maxinfpercentage
 FROM [dbo].[CovidDeaths]
---Where location like '%india%'
-Group by location,population
-order by maxinfpercentage desc
+-- WHERE location LIKE '%india%'
+GROUP BY location, population
+ORDER BY maxinfpercentage DESC;
 
---Countries with highsest death per population
-SELECT location,MAX(cast(total_deaths as int)) as HighestDeathCount
---MAX((total_deaths/total_cases)*100) as maxdeathpercentage
+-- Finding countries with the highest death count per population and ordering by the highest death count in descending order
+SELECT location, MAX(CAST(total_deaths AS INT)) AS HighestDeathCount
 FROM [dbo].[CovidDeaths]
---Where location like '%india%'
-where continent is not null
-Group by location
-order by HighestDeathCount desc
+-- WHERE location LIKE '%india%'
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY HighestDeathCount DESC;
 
---in continents
-SELECT continent,MAX(cast(total_deaths as int)) as HighestDeathCount
---MAX((total_deaths/total_cases)*100) as maxdeathpercentage
+-- Finding continents with the highest death count and ordering by the highest death count in descending order
+SELECT continent, MAX(CAST(total_deaths AS INT)) AS HighestDeathCount
 FROM [dbo].[CovidDeaths]
---Where location like '%india%'
-where continent is not null
-Group by continent
-order by HighestDeathCount desc
+-- WHERE location LIKE '%india%'
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY HighestDeathCount DESC;
 
-Select MAx(Cast(total_deaths as int)) as sum
+-- Finding the maximum death count in America continent
+SELECT MAX(CAST(total_deaths AS INT)) AS sum
 FROM [dbo].[CovidDeaths]
-where continent like '%America%'
-group by continent
+WHERE continent LIKE '%America%'
+GROUP BY continent;
 
---Global numbers
-SELECT date,Sum(new_cases) as totalcases,
-sum(cast(new_deaths as int)) as totaldeaths,
-(sum(cast(new_deaths as int))/Sum(new_cases))*100 as deathpercentage
+-- Finding global COVID statistics including total cases, total deaths, and death percentage, and ordering by date
+SELECT date, SUM(new_cases) AS totalcases,
+SUM(CAST(new_deaths AS INT)) AS totaldeaths,
+(SUM(CAST(new_deaths AS INT)) / SUM(new_cases)) * 100 AS deathpercentage
 FROM [dbo].[CovidDeaths]
-Where continent is not null
-Group by date
-order by 1,2
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY 1, 2;
 
+-- Joining CovidDeaths and CovidVaccinations tables to calculate vaccination statistics
+SELECT D.continent, D.location, D.date, D.population, V.new_vaccinations,
+SUM(CAST(V.new_vaccinations AS INT)) 
+OVER (PARTITION BY D.location ORDER BY D.date, D.location) AS RollingPeopleVaccinated
+FROM [dbo].[CovidDeaths] D
+JOIN [dbo].[CovidVaccinations] V
+ON D.location = V.location
+AND D.date = V.date
+WHERE D.continent IS NOT NULL
+AND D.population IS NOT NULL
+ORDER BY 2, 3;
 
---Vaccinations
---total population vs vaccination
-Select D.continent,D.location,D.date,D.population,V.new_vaccinations,
-SUM(Cast(V.new_vaccinations as int)) 
-over (partition by D.location order by D.date,D.location) as RollingPeopleVaccinated
-From [dbo].[CovidDeaths] D
- Join [dbo].[CovidVaccinations] V
-on D.location=V.location
-and D.date=V.date
-WHERE 
-    D.continent IS NOT NULL
-    AND D.population IS NOT NULL
-order by 2,3
-
---Using cte
+-- Using common table expression (CTE) to calculate vaccination percentages
 WITH popvsvac (Continent, Location, Date, Population, New_vaccinations, RollingPeopleVaccinated)
 AS (
-    SELECT
-        D.continent,
-        D.location,
-        D.date,
-        D.population,
-        V.new_vaccinations,
-        SUM(CAST(V.new_vaccinations AS INT)) OVER (PARTITION BY D.location ORDER BY D.date, D.location) AS RollingPeopleVaccinated
-    FROM
-        [dbo].[CovidDeaths] D
-    JOIN
-        [dbo].[CovidVaccinations] V
-    ON
-        D.location = V.location
-        AND D.date = V.date
-    WHERE
-        D.continent IS NOT NULL
-        AND D.population IS NOT NULL
+    SELECT D.continent, D.location, D.date, D.population, V.new_vaccinations,
+    SUM(CAST(V.new_vaccinations AS INT)) OVER (PARTITION BY D.location ORDER BY D.date, D.location) AS RollingPeopleVaccinated
+    FROM [dbo].[CovidDeaths] D
+    JOIN [dbo].[CovidVaccinations] V
+    ON D.location = V.location
+    AND D.date = V.date
+    WHERE D.continent IS NOT NULL
+    AND D.population IS NOT NULL
 )
-SELECT
-    *,
-    (RollingPeopleVaccinated / Population) * 100 AS percentage
-FROM
-    popvsvac;
+SELECT *, (RollingPeopleVaccinated / Population) * 100 AS percentage
+FROM popvsvac;
 
-
---using temp table
-Drop table if exists #PercentPopulationVacinated
-Create table #PercentPopulationVacinated
+-- Using temporary table to store vaccination data and calculating vaccination percentages
+DROP TABLE IF EXISTS #PercentPopulationVaccinated;
+CREATE TABLE #PercentPopulationVaccinated
 (
-Continent nvarchar(255),
-Location nvarchar(255),
-Date datetime,
-Population numeric,
-New_vaccinations numeric,
-RollingPeopleVaccinated numeric
-)
-Insert into #PercentPopulationVacinated
-Select D.continent,D.location,D.date,D.population,V.new_vaccinations,
-SUM(Cast(V.new_vaccinations as int)) 
-over (partition by D.location order by D.date,D.location) as RollingPeopleVaccinated
-From [dbo].[CovidDeaths] D
- Join [dbo].[CovidVaccinations] V
-on D.location=V.location
-and D.date=V.date
-WHERE 
-    D.continent IS NOT NULL
-    AND D.population IS NOT NULL
-order by 2,3
-SELECT
-    *,
-    (RollingPeopleVaccinated / Population) * 100 AS percentage
-FROM
-#PercentPopulationVacinated
+    Continent nvarchar(255),
+    Location nvarchar(255),
+    Date datetime,
+    Population numeric,
+    New_vaccinations numeric,
+    RollingPeopleVaccinated numeric
+);
+INSERT INTO #PercentPopulationVaccinated
+SELECT D.continent, D.location, D.date, D.population, V.new_vaccinations,
+SUM(CAST(V.new_vaccinations AS INT)) 
+OVER (PARTITION BY D.location ORDER BY D.date, D.location) AS RollingPeopleVaccinated
+FROM [dbo].[CovidDeaths] D
+JOIN [dbo].[CovidVaccinations] V
+ON D.location = V.location
+AND D.date = V.date
+WHERE D.continent IS NOT NULL
+AND D.population IS NOT NULL
+ORDER BY 2, 3;
+SELECT *, (RollingPeopleVaccinated / Population) * 100 AS percentage
+FROM #PercentPopulationVaccinated;
 
+-- Creating a view to encapsulate the population vs vaccination data
+CREATE VIEW PercentPopulationVaccinated AS
+SELECT D.continent, D.location, D.date, D.population, V.new_vaccinations,
+SUM(CAST(V.new_vaccinations AS INT)) 
+OVER (PARTITION BY D.location ORDER BY D.date, D.location) AS RollingPeopleVaccinated
+FROM [dbo].[CovidDeaths] D
+JOIN [dbo].[CovidVaccinations] V
+ON D.location = V.location
+AND D.date = V.date
+WHERE D.continent IS NOT NULL
+AND D.population IS NOT NULL;
 
---creating view for tableau
-
-Create View PercentPopulationVaccinated as
-Select D.continent,D.location,D.date,D.population,V.new_vaccinations,
-SUM(Cast(V.new_vaccinations as int)) 
-over (partition by D.location order by D.date,D.location) as RollingPeopleVaccinated
-From [dbo].[CovidDeaths] D
- Join [dbo].[CovidVaccinations] V
-on D.location=V.location
-and D.date=V.date
-WHERE 
-    D.continent IS NOT NULL
-    AND D.population IS NOT NULL
---order by 2,3
-
-Select *From
-PercentPopulationVaccinated
+-- Querying the created view
+SELECT *
+FROM PercentPopulationVaccinated;
